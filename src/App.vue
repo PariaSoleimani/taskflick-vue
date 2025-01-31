81d070c1f3712ff50ee2cdbcd9ff1893331d5a2e
PariaSoleimani <paria.slmni@gmail.com>
2025-01-31 13:52:22 +0330

    feat(nav): add sidebar navigation and implement search input

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<template></template>
+<template>
+  <div class="mx-auto flex h-screen max-w-7xl items-start gap-3 p-5">
+    <side-bar></side-bar>
+  </div>
+</template>
 
 <script>
+  import SideBar from './components/SideBar.vue';
+
   export default {
+    components: {SideBar},
     data() {
       return {};
     },
---
+++ src/components/SideBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <aside class="flex h-full w-64 flex-col gap-5 rounded-lg bg-zinc-100 p-5">
+    <h1 class="font-serif text-3xl">NoteFlick</h1>
+    <nav>
+      <form class="flex w-full items-center gap-1 rounded-lg bg-zinc-200 px-1.5 py-1">
+        <input
+          type="text"
+          placeholder="Search"
+          class="w-full text-zinc-800 placeholder:text-zinc-400 focus:outline-none"
+        />
+        <button class="flex cursor-pointer items-center justify-center" type="submit">
+          <i class="ph ph-magnifying-glass text-lg text-zinc-400"></i>
+        </button>
+      </form>
+    </nav>
+  </aside>
+</template>
+
+<script></script>
