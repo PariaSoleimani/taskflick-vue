b1c752ee87c44ac64e33bbe6e95476b32f8e4867
PariaSoleimani <paria.slmni@gmail.com>
2025-01-31 12:46:47 +0330

    chore(setup): add initial project structure with config files and index.html

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{html,js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Cabin', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
